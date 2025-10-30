2f1030d4aaf589771da67df043e963dd62e2383c
ForecastSnow <tomasperezpsn@outlook.com>
2025-10-30 19:34:21 -0300

    VITE_BASE_PUBLIC_PATCH

--- front-ForeIA/vite.config.ts
+++ front-ForeIA/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import vitePluginSvgr from 'vite-plugin-svgr'
 
 
-export default defineConfig({
-  plugins: [vitePluginSvgr(), react(), tailwindcss()],
-  /* server: {
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vitePluginSvgr(), react(), tailwindcss()],
+
+    base: env.VITE_BASE_PUBLIC_PATH || '/',
     
-    host: '0.0.0.0', 
-    port: 5173,
-  } */
+    /* server: {
+      host: '0.0.0.0', 
+      port: 5173,
+    } */
+  }
 })
